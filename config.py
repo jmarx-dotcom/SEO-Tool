8b71bfccac4000a5bc094825be3e0da78864e7ed
jmarx-dotcom <j.marx@goettinger-tageblatt.de>
2025-11-26 13:02:02 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Hier trägst du die RSS-Feeds ein, aus denen das Tool Artikel ziehen soll.
+# Für den Start nimm ruhig nur 1–2 Feeds.
+
+FEEDS = [
+    "https://www.goettinger-tageblatt.de/arc/outboundfeeds/rss/category/lokales/goettingen-lk/goettingen/",  # <- diesen Link durch euren echten Feed ersetzen
+    # weitere Feeds kannst du später ergänzen
+]
